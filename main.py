25a90d9f9e874fd692db65d34402c992fa77ed25
zilon4eg <hdd500t@yandex.ru>
2022-03-16 13:31:56 +0300

    work v3

--- main.py
+++ main.py
@@ -2,81 +2,151 @@ import subprocess
 import shutil
 import os
 from threading import Thread
+from pprint import pprint
 
 
-def get_pc_name_list():
+def ping(hostname):
+    try:
+        cmd_line = f'ping {hostname}'
+        p = subprocess.Popen(cmd_line, shell=True, stdout=subprocess.PIPE)
+
+        response = [line_data.decode("cp866", "ignore").rstrip().lower() for line_data in p.stdout]
+        response = list(string for string in response if 'ответ' in string or 'reply' in response)
+        # print(response)
+        count = 0
+        for string in response:
+            if 'time' in string or 'время' in string:
+                count += 1
+        status = False if count < 3 else True
+        if status:
+            return {'name': 'ping', 'status': status, 'message': f'ok'}
+        else:
+            return {'name': 'ping', 'status': status, 'message': f'узел недоступен'}
+
+    except subprocess.CalledProcessError as err:
+        return {'name': 'ping', 'status': False, 'message': err}
+    except FileNotFoundError as err:
+        return {'name': 'ping', 'status': False, 'message': err}
+
+
+def get_hostname_list():
     split_string = r'"\r\n"'
-    b_comp_list = subprocess.check_output(['dsquery', 'computer', 'domainroot', '-scope', 'subtree', '-limit', '0'])
+    b_host_list = subprocess.check_output(['dsquery', 'computer', 'domainroot', '-scope', 'subtree', '-limit', '0'])
     # получаем список элементов AD с параметрами CN, OU, DC
-    comp_list = str(b_comp_list)[3:-6].split(split_string)
+    host_list = str(b_host_list)[3:-6].split(split_string)
     # pprint(comp_list)
     # получаем список СОКРАЩЕННЫХ имен ПК из каталога AD OU=SKA-Computers
-    comp_list = sorted(list(list(comp.split(','))[0][3:] for comp in comp_list if 'ou=ska-computers' in comp.lower()))
-    print(f'Найдено {len(comp_list)} ПК')
-    return comp_list
+    host_list = sorted(list(list(comp.split(','))[0][3:] for comp in host_list if 'ou=ska-computers' in comp.lower()))
+    return host_list
 
 
 def path_check_create(path):
     try:
         if not os.path.exists(path):
-            print(f'{" " * indent_size}Путь "{path}" не существует')
-
             try:
                 os.makedirs(path)
-                print(f'{" " * indent_size}Путь "{path}" создан')
+                return {'name': 'path', 'status': True, 'message': f'Путь "{path}" создан'}
             except Exception as err:
-                error = str(err).replace('\\\\', '\\').replace("'", '"')
-                print(f'{" " * indent_size}{error}')
+                error = str(err).replace("'", '"')
+                return {'name': 'path', 'status': False, 'message': error}
 
         else:
-            print(f'{" " * indent_size}Доступность пути "{path}" проверена')
+            return {'name': 'path', 'status': True, 'message': f'ok'}
     except Exception as err:
-        print(err)
+        return {'name': 'path', 'status': False, 'message': err}
 
 
-def copy_nethasp_ini(pc_name):
+def copy_nethasp_ini(hostname):
     # for pc_name in pc_name_list:
     file_name = r'nethasp.ini'
     path_copy = r'\\fs\SHARE\Documents\PUBLIC\Soft\1C\config\conf'
-    path_paste = f'\\\\{pc_name}\\C$\\Program Files\\1cv8\\conf'
+    path_paste = f'\\\\{hostname}\\C$\\Program Files\\1cv8\\conf'
     # проверяем наличие каталогов, если их нет - создаем.
     path_check_create(path_paste)
     # копируем файл
     try:
         shutil.copyfile(f'{path_copy}\\{file_name}', f'{path_paste}\\{file_name}')
-        print(f'{" " * indent_size}Файл "{file_name}" успешно скопирован')
+        return {'name': 'nethasp.ini', 'status': True, 'message': f'ok'}
     except Exception as err:
-        error = str(err).replace('\\\\', '\\').replace("'", '"')
-        print(f'{" " * indent_size}{error}')
+        error = str(err).replace("'", '"')
+        return {'name': 'nathasp.ini', 'status': False, 'message': error}
 
 
-def copy_1cestart_cfg(pc_name):
+def copy_1cestart_cfg(hostname):
     file_name = r'1cestart.cfg'
     path_copy = r'\\fs\SHARE\Documents\PUBLIC\Soft\1C\config\1CEStart'
-    path_users_dir = f'\\\\{pc_name}\\C$\\Users'
-    for user_dir in list(dir_name for dir_name in os.listdir(path_users_dir) if dir_name not in ['AMS User', 'Default', 'docflow', 'Public', 'All Users', 'Default User', 'Администратор', 'Все пользователи'] and not os.path.isfile(f'{path_users_dir}\\{dir_name}')):
-        print(f'Профиль пользователя: {user_dir}')
-        path_paste = f'{path_users_dir}\\{user_dir}\\AppData\\Roaming\\1C\\1CEStart'
-        path_check_create(path_paste)
-        try:
-            shutil.copyfile(f'{path_copy}\\{file_name}', f'{path_paste}\\{file_name}')
-            print(f'{" " * indent_size}Файл "{file_name}" для "{user_dir}" успешно скопирован')
-        except Exception as err:
-            error = str(err).replace('\\\\', '\\').replace("'", '"')
-            print(f'{" " * indent_size}{error}')
+    path_users_dir = f'\\\\{hostname}\\C$\\Users'
+    default_account = ['AMS User', 'Default', 'docflow', 'Public', 'All Users', 'Default User', 'Администратор', 'Все пользователи']
+    try:
+        user_dirs = list(dir_name for dir_name in os.listdir(path_users_dir) if dir_name not in default_account and not os.path.isfile(f'{path_users_dir}\\{dir_name}'))
+    except Exception as err:
+        user_dirs = False
+        error = str(err).replace("'", '"')
+        return {'name': '1cestart.cfg', 'status': False, 'message': error}
+
+    if user_dirs:
+        result = []
+        for user_dir in user_dirs:
+            path_paste = f'{path_users_dir}\\{user_dir}\\AppData\\Roaming\\1C\\1CEStart'
+            path_check_create(path_paste)
+            try:
+                shutil.copyfile(f'{path_copy}\\{file_name}', f'{path_paste}\\{file_name}')
+                result.append({'name': '1cestart.cfg', 'status': True, 'message': f'{user_dir} - ok'})
+            except Exception as err:
+                error = str(err).replace("'", '"')
+                result.append({'name': '1cestart.cfg', 'status': False, 'message': f'{user_dir} - {error}'})
+        return result
+
+
+def body(hostname):
+    local_output = []
+    local_output.append({'hostname': hostname, 'status': True})
+    ping_resp = ping(hostname)
+    local_output.append(ping_resp)
+    if ping_resp['status']:
+        local_output.append(copy_nethasp_ini(hostname))
+        [local_output.append(message) for message in copy_1cestart_cfg(hostname)]
+    output.append(local_output)
+
+
+def print_data(data):
+    for host in data:
+        for count, line in enumerate(host, 1):
+            if count == 1:
+                print(line['hostname'])
+            if count > 1:
+                name = line['name']
+                message = line['message'].replace('\\\\', '\\')
+                print(f'{" " * 3}{name} - {message}')
+
 
 if __name__ == '__main__':
-    indent_size = 2
+    output = []
+    hostname_list = get_hostname_list()
+    hostname_list = ['SKA-SUHORUKOV', 'SKA-TESTING', 'SKA-BUH']  # заглушка
 
-    pc_name_list = get_pc_name_list()
-    # pc_name_list = ['SKA-SUHORUKOV']  # заглушка
+    for pc_number, hostname in enumerate(hostname_list, 1):
+        Thread(target=body, args=(hostname, )).start()
 
-    for pc_number, pc_name in enumerate(pc_name_list, 1):
-        markup = f'=({pc_number:0{len(str(len(pc_name_list)))}})=({pc_name})'
-        markup = f'{markup}{"=" * (40 - len(markup))}'
-        print(markup)
-        print('Копируем файл конфигурации nethasp.ini')
-        copy_nethasp_ini(pc_name)
-        print(f'{"-" * 40}')
-        print('Копируем файл конфигурации 1cestart.cfg')
-        copy_1cestart_cfg(pc_name)
+    while True:
+        if len(output) == len(hostname_list):
+            complete = []
+            error = []
+            # pprint(output)
+
+            for host in output:
+                # msg_list = list(list(dictionary.values()) for dictionary in msg)
+                all_status = []
+                for line in host:
+                    all_status.append(line['status'])
+
+                if False in all_status:
+                    error.append(host)
+                else:
+                    complete.append(host)
+
+            print('======== COMPLETE ========')
+            print_data(complete)
+            print('======== ERROR ========')
+            print_data(error)
+            break
--- test.py
+++ test.py
@@ -1,19 +1,28 @@
 import subprocess
-from pprint import pprint
 
 
-try:
-    cmd_line = "ping SKA-TESTING"
-    p = subprocess.Popen(cmd_line, shell=True, stdout=subprocess.PIPE)
+def ping(hostname):
+    try:
+        cmd_line = f'ping {hostname}'
+        p = subprocess.Popen(cmd_line, shell=True, stdout=subprocess.PIPE)
 
-    # response = ''
-    # for line_data in p.stdout:
-    response = [line_data.decode("cp866", "ignore").rstrip() for line_data in p.stdout]
-    pprint(response)
-    # p.wait()
-    # print(p.returncode)  # =1 если  сайт не существует.   0 - если все в порядке.
+        response = [line_data.decode("cp866", "ignore").rstrip().lower() for line_data in p.stdout]
+        response = list(string for string in response if 'ответ' in string or 'reply' in response)
+        # print(response)
+        count = 0
+        for string in response:
+            if 'time' in string or 'время' in string:
+                count += 1
+        status = False if count < 3 else True
+        if status:
+            return {'status': status, 'message': f'OK'}
+        else:
+            return {'status': status, 'message': f'Узел "{hostname}" недоступен'}
 
-except subprocess.CalledProcessError as e:
-    print(e)
-except FileNotFoundError as e:
-    print(e)
+    except subprocess.CalledProcessError as err:
+        return {'status': False, 'message': err}
+    except FileNotFoundError as err:
+        return {'status': False, 'message': err}
+
+
+print(ping('SKA-TESTING'))
